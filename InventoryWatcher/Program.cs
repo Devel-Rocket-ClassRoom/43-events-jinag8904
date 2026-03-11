874123616c6254993219640d52ca954aac32067f
jinag8904 <jinag8904@gmail.com>
2026-03-11 15:13:58 +0900

    실습 - 이벤트 (인벤토리)

--- InventoryWatcher/Program.cs
+++ InventoryWatcher/Program.cs
@@ -1,4 +1,70 @@
 using System;
+using System.Collections.Generic;
 
-// README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Main();
+
+static void Main()
+{
+    Inventory inventory = new Inventory();
+    InventoryUI ui = new InventoryUI();
+    AutoBuyer autoBuyer = new AutoBuyer();
+
+    inventory.ItemChanged += ui.Print;
+    inventory.ItemChanged += autoBuyer.Print;
+
+    inventory.AddItem("포션", 5);
+    inventory.AddItem("화살", 10);
+    inventory.AddItem("포션", 3);
+    inventory.RemoveItem("화살", 7);
+    inventory.RemoveItem("화살", 5);
+}
+
+class Inventory // 게시자
+{
+    Dictionary<string, int> inventory = new Dictionary<string, int> { };
+
+    public event Action<string, int, int> ItemChanged;
+
+    public void AddItem(string name, int count)
+    {
+        int oldCount;
+
+        if(inventory.TryAdd(name, count))
+        {
+            oldCount = 0;
+        }
+
+        else
+        {
+            oldCount = inventory[name];
+            inventory[name] += count;
+        }
+
+        ItemChanged?.Invoke(name, oldCount, inventory[name]);
+    }
+
+    public void RemoveItem(string name, int count)
+    {
+        int oldCount = inventory[name];
+        inventory[name] -= count;
+        if (inventory[name] < 0) inventory[name] = 0;
+        ItemChanged?.Invoke(name, oldCount, inventory[name]);
+    }
+}
+
+class InventoryUI   // 구독자1
+{
+    public void Print(string name, int oldCount, int newCount)
+    {
+        Console.WriteLine($"[UI] {name}: {oldCount} -> {newCount}");
+    }
+}
+
+class AutoBuyer // 구독자2
+{
+    public void Print(string name, int oldCount, int newCount)
+    {
+        if (newCount == 0)
+            Console.WriteLine($"[자동구매] {name} 재고 소진! 자동 구매 요청");
+    }
+}
